bb5fd66cf2a891a492240aff9b63c1bda6ce90e9
Pouria Delfanazari <pdelfan@gmail.com>
2023-11-08 11:35:03 -0800

    Update feeds loading skeleton to match design

--- src/app/dashboard/feeds/loading.tsx
+++ src/app/dashboard/feeds/loading.tsx
@@ -10,11 +10,13 @@ export default function Loading() {
         <SavedFeedListSkeleton />
       </section>
       <section>
-        <h2 className="text-2xl font-semibold mx-3 sm:mx-0 mb-2">
-          Popular feeds
-        </h2>
-        <div className="flex flex-col gap-2">
-          <Search placeholder="Search for feeds" />
+        <div>
+          <div className="flex flex-wrap justify-between gap-x-12 gap-y-2 mb-2">
+            <h2 className="flex-auto text-2xl font-semibold mx-3 sm:mx-0 mb-2">
+              Popular feeds
+            </h2>
+            <Search placeholder="Search for feeds" />
+          </div>
           <FeedListSkeleton />
         </div>
       </section>
