26d6b2f6bcc48c673bb57bb7586dd128e3716988
Pouria Delfanazari <pdelfan@gmail.com>
2023-11-15 14:48:21 -0800

    Update suggestions loading skeleton

--- src/app/dashboard/search/loading.tsx
+++ src/app/dashboard/search/loading.tsx
@@ -1,12 +1,20 @@
 import ProfileCardSkeleton from "@/components/contentDisplay/profileCard/ProfileCardSkeleton";
+import Search from "@/components/filter/search/Search";
 
 export default function Loading() {
   return (
-    <section>
-      <h2 className="text-2xl font-semibold px-3 sm:px-0 mb-2">
-        Suggestions
-      </h2>
-      <ProfileCardSkeleton />
+    <section className="flex flex-col gap-5">
+      <section>
+        <h2 className="text-2xl font-semibold px-3 sm:px-0 mb-2">Search</h2>
+        <Search placeholder="Search for users or posts" />
+      </section>
+
+      <section>
+        <h2 className="text-2xl font-semibold px-3 sm:px-0 mb-2">
+          Suggestions
+        </h2>
+        <ProfileCardSkeleton />
+      </section>
     </section>
   );
 }
