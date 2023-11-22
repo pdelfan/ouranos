f59033e07bca661603d3ece9ba6e24f44c70d0c6
Pouria Delfanazari <pdelfan@gmail.com>
2023-11-22 00:00:07 -0800

    Redirect to post after clicking notification content

--- src/components/contentDisplay/notification/NotificationContent.tsx
+++ src/components/contentDisplay/notification/NotificationContent.tsx
@@ -5,6 +5,9 @@ import { getPost } from "@/lib/api/bsky/feed";
 import PostText from "@/components/dataDisplay/postText/postText";
 import { AppBskyFeedDefs } from "@atproto/api";
 import PostEmbed from "@/components/dataDisplay/postEmbed/PostEmbed";
+import Link from "next/link";
+import { getPostId } from "@/lib/utils/link";
+import { useRouter } from "next/navigation";
 
 interface Props {
   uri: string;
@@ -13,8 +16,8 @@ interface Props {
 
 export default function NotificationContnet(props: Props) {
   const { uri, filter } = props;
-
   const agent = useAgent();
+  const router = useRouter();
 
   const { status, data, error, isLoading, isFetching } = useQuery({
     queryKey: ["notificationContent", uri],
@@ -25,7 +28,15 @@ export default function NotificationContnet(props: Props) {
     data?.data.thread && (data.data.thread as AppBskyFeedDefs.FeedViewPost);
 
   return (
-    <div>
+    <div
+      onClick={(e) => {
+        e.stopPropagation();
+        router.push(
+          `/dashboard/user/${post?.post.author.handle}/post/${getPostId(uri)}`
+        );
+      }}
+      className="cursor-pointer"
+    >
       {post && <PostText record={post.post.record} />}
       {post?.post.embed && (
         <div className="max-w-xs">
