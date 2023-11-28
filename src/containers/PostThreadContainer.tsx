df956de1e3eb4dc56d50c94d9f0e7c3e97cbd573
Pouria Delfanazari <pdelfan@gmail.com>
2023-11-27 23:45:57 -0800

    Add a function for grouping all reply threads (WIP)

--- src/containers/PostThreadContainer.tsx
+++ src/containers/PostThreadContainer.tsx
@@ -3,10 +3,16 @@
 import useAgent from "@/lib/hooks/bsky/useAgent";
 import { getPostThread } from "@/lib/api/bsky/feed";
 import { useQuery } from "@tanstack/react-query";
+import { AppBskyFeedDefs } from "@atproto/api";
 
 interface Props {
   id: string;
   handle: string;
+}
+
+interface Reply {
+  post: AppBskyFeedDefs.ThreadViewPost;
+  replies: Reply[];
 }
 
 export default function PostThreadContainer(props: Props) {
@@ -27,6 +33,48 @@ export default function PostThreadContainer(props: Props) {
     },
   });
 
+  if (thread.data) {
+    const replies =
+      (thread.data.replies as AppBskyFeedDefs.ThreadViewPost[]) || [];
+    if (replies.length > 0) {
+      const getConnectedReplies = (
+        post: AppBskyFeedDefs.ThreadViewPost,
+        currentChain: AppBskyFeedDefs.ThreadViewPost[] = []
+      ) => {
+        currentChain.push(post);
+
+        // Check if the post has replies
+        if (post.replies && post.replies.length > 0) {
+          let longestChain = currentChain;
+
+          // Iterate through replies and call the function recursively
+          for (const reply of post.replies) {
+            if (AppBskyFeedDefs.isThreadViewPost(reply)) {
+              const chain = getConnectedReplies(reply, currentChain.slice());
+
+              // Update the longest chain if the new one is longer
+              if (chain.length > longestChain.length) {
+                longestChain = chain;
+              }
+            }
+          }
+
+          return longestChain;
+        }
+
+        return currentChain;
+      };
+
+      // call func
+      const chains = replies.map((reply) => getConnectedReplies(reply));
+
+      const longestChain = replies
+        .map((reply) => getConnectedReplies(reply))
+        .reduce((acc, chain) => (chain.length > acc.length ? chain : acc), []);
+
+      console.log(chains);
+    }
+  }
 
   return <></>;
 }
