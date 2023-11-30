fa3d7f37595bc435a890454856c3c22eadfe4451
Pouria Delfanazari <pdelfan@gmail.com>
2023-11-30 00:54:00 -0800

    Update SavedFeedListSkeleton.tsx

--- src/components/contentDisplay/savedFeedList/SavedFeedListSkeleton.tsx
+++ src/components/contentDisplay/savedFeedList/SavedFeedListSkeleton.tsx
@@ -2,7 +2,7 @@ function Skeleton() {
   return (
     <article className="animate-pulse flex justify-between items-center gap-2 p-3 border sm:first:rounded-t-2xl sm:last:rounded-b-2xl last:border-b even:[&:not(:last-child)]:border-b-0 odd:[&:not(:last-child)]:border-b-0">
       <div className="flex flex-wrap gap-3 items-center">
-        <div className="rounded-lg bg-slate-200 h-10 w-10"></div>
+        <div className="rounded-lg bg-slate-200 h-8 w-8"></div>
         <div className="bg-slate-200 w-32 h-3" />
       </div>
     </article>
