b98bf4223b46bd1039e9ed63af3bb6483a76dc7a
Pouria Delfanazari <pdelfan@gmail.com>
2023-12-05 14:59:41 -0800

    Hide and show app bar on scroll

--- src/components/navigational/appBar/AppBar.tsx
+++ src/components/navigational/appBar/AppBar.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { useSession } from "next-auth/react";
 import NavItem from "../navbar/NavItem";
+import useHideOnScroll from "@/lib/hooks/useHideOnScroll";
 
 export default function AppBar() {
   const pathname = usePathname();
-  const { data: session } = useSession();
+  const show = useHideOnScroll();
 
   return (
-    <nav className="flex justify-between py-4 px-6 border-t gap-6 fixed bottom-0 z-40 w-full md:hidden bg-white">
+    <nav
+      className={`flex justify-between py-4 px-6 border-t gap-6 fixed bottom-0 z-40 w-full md:hidden bg-white ${
+        show ? "translate-y-0" : "translate-y-36"
+      } transition-translate ease-in-out duration-300`}
+    >
       <NavItem
         href="/dashboard/home"
         icons={["bxs:home", "bx:home"]}
