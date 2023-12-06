a8d62566429c0984a00af59ca4e19e72bc49a3b7
Pouria Delfanazari <pdelfan@gmail.com>
2023-12-06 15:20:07 -0800

    Update content label styling on top bar, remove focus from compose button

--- src/components/inputs/editor/TopEditorBar.tsx
+++ src/components/inputs/editor/TopEditorBar.tsx
@@ -20,10 +20,12 @@ export default function TopEditorBar(props: Props) {
       {label !== "" && (
         <Button
           onClick={onRemoveLabel}
-          icon="material-symbols:no-adult-content"
-          className="px-2 py-1.5 border-2 border-orange-500 rounded-full text-sm text-orange-500 font-medium hover:bg-orange-500 hover:text-white"
+          icon="octicon:shield-16"
+          className="px-2 py-1.5 border-2 border-orange-500 rounded-full text-sm text-orange-500 font-semibold hover:bg-orange-500 hover:text-white"
         >
-          {label}
+          {label === "nsfw"
+            ? "Porn"
+            : label.charAt(0).toUpperCase() + label.slice(1)}
         </Button>
       )}
       <Button className="bg-primary text-white text-sm font-semibold px-6 py-2 rounded-full hover:bg-primary-dark">
