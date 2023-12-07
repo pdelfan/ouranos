85ef4f974d606cb2762dca68b53ac699c7de71e5
Pouria Delfanazari <pdelfan@gmail.com>
2023-12-06 22:51:50 -0800

    Update refetch and compose button stylings

--- src/components/actions/refetch/Refetch.tsx
+++ src/components/actions/refetch/Refetch.tsx
@@ -15,7 +15,7 @@ export default function Refetch(props: Props) {
       icon="tabler:refresh"
       iconSize="text-2xl"
       iconColor="text-neutral-500 hover:text-neutral-600"
-      className={`z-50 p-3 rounded-full fixed bottom-20 md:bottom-5 ml-3 border bg-neutral-50 shadow-lg border-neutral-200 hover:bg-neutral-100 ${
+      className={`z-50 p-3 rounded-full fixed bottom-20 md:bottom-5 ml-3 border-2 border-neutral-300 bg-white shadow hover:bg-neutral-50 hover:rotate-180 ${
         show ? "translate-y-0" : "translate-y-36"
       } transition-translate ease-in-out duration-300`}
     />
