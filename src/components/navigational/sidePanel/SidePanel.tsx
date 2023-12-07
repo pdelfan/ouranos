6c73d81c15098bd98123dc04e584d88dc4078d85
Pouria Delfanazari <pdelfan@gmail.com>
2023-12-06 19:49:41 -0800

    Turn sidepanel's logo into scroll to top button

--- src/components/navigational/sidePanel/SidePanel.tsx
+++ src/components/navigational/sidePanel/SidePanel.tsx
@@ -4,11 +4,15 @@ import Link from "next/link";
 import Navbar from "../navbar/Navbar";
 import Image from "next/image";
 import ComposeButton from "@/components/actions/composeButton/ComposeButton";
+import Button from "@/components/actions/button/Button";
 
 export default function SidePanel() {
   return (
     <menu className="hidden md:inline-flex items-center lg:items-start flex-col sticky  top-6 h-full">
-      <Link href="/dashboard/home" className="inline mb-12">
+      <Button
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        className="inline mb-12"
+      >
         <Image
           src="/ouranos.svg"
           alt="Ouranos logo"
@@ -17,7 +21,7 @@ export default function SidePanel() {
           priority
           className="block transition-transform ease-in-out duration-700 hover:rotate-180"
         />
-      </Link>
+      </Button>
       <Navbar />
       <div className="mt-28">
         <ComposeButton mode="fixed" />
