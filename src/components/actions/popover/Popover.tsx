5c9632b948643b6a945ea5a2a366f25780d4eac2
Pouria Delfanazari <pdelfan@gmail.com>
2023-12-07 12:27:58 -0800

    Fix overflow issues for dropdown and popover

--- src/components/actions/popover/Popover.tsx
+++ src/components/actions/popover/Popover.tsx
@@ -46,7 +46,7 @@ function PopoverContent({ children }: { children: ReactNode }) {
               e.preventDefault(); // remove outline on close
             }}
             align="center"
-            className="z-50 min-w-fit mt-2 mb-2 bg-white rounded-xl p-1 drop-shadow-lg border max-h-44 overflow-y-scroll"
+            className="z-50 min-w-fit mt-2 mb-2 bg-white rounded-xl p-1 drop-shadow-lg border max-h-44 overflow-y-hidden hover:overflow-y-auto"
           >
             {children}
           </RadixPopover.Content>
