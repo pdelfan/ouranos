df0aa481f12e6aed29dcfdbd8f340046048ef02b
Pouria Delfanazari <pdelfan@gmail.com>
2023-12-09 18:46:46 -0800

    Update Refetch.tsx

--- src/components/actions/refetch/Refetch.tsx
+++ src/components/actions/refetch/Refetch.tsx
@@ -15,7 +15,7 @@ export default function Refetch(props: Props) {
       icon="tabler:refresh"
       iconSize="text-2xl"
       iconColor="text-neutral-500 hover:text-neutral-600"
-      className={`z-50 p-3 rounded-full fixed bottom-20 md:bottom-5 ml-3 border-2 border-neutral-300 bg-white hover:bg-neutral-50 hover:rotate-180 ${
+      className={`z-50 p-3 rounded-full fixed bottom-20 md:bottom-5 ml-3 border-2 border-neutral-300 bg-white hover:bg-neutral-50 hover:-rotate-180 ${
         show ? "translate-y-0" : "translate-y-36"
       } transition-translate ease-in-out duration-300`}
     />
