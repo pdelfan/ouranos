0ed628b5ae7e0130cc76cd6088b21d5332aeaa34
Pouria Delfanazari <pdelfan@gmail.com>
2023-12-15 23:12:38 -0800

    Add icons and social share images

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Ouranos",
+    short_name: "Ouranos",
+    description: "Your friendly Bluesky client for the web",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}
