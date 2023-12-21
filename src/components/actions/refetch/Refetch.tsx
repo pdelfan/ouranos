a76b84b12a133d52eacef31f91ae65e977565c5c
Pouria Delfanazari <pdelfan@gmail.com>
2023-12-21 12:05:34 -0800

    Remove smooth scrolling on page nav, adjust refetch button border

--- src/components/actions/refetch/Refetch.tsx
+++ src/components/actions/refetch/Refetch.tsx
@@ -19,7 +19,7 @@ export default function Refetch(props: Props) {
       icon="tabler:refresh"
       iconSize="text-2xl"
       iconColor="text-neutral-500 hover:text-neutral-600"
-      className={`z-40 p-3 rounded-full fixed bottom-28 sm:bottom-24 md:bottom-8 ml-3 border border-neutral-300 bg-white hover:bg-neutral-50 hover:-rotate-180 ${
+      className={`z-40 p-3 rounded-full fixed bottom-28 sm:bottom-24 md:bottom-8 ml-3 border-2 border-neutral-300 bg-white hover:bg-neutral-50 hover:-rotate-180 ${
         show ? "translate-y-0 opacity-100" : "translate-y-36 opacity-0"
       } transition-all ease-in-out duration-500`}
     />
