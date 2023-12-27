6f22591d7e2b39f6bde7d10fe146437df0011727
Pouria Delfanazari <pdelfan@gmail.com>
2023-12-26 18:26:21 -0800

    Update screenshots and compose/refetch button positions

--- src/components/actions/composeButton/ComposeButton.tsx
+++ src/components/actions/composeButton/ComposeButton.tsx
@@ -18,7 +18,7 @@ export default function ComposeButton(props: Props) {
       {mode === "float" && (
         <button
           onClick={() => openComposer()}
-          className={`z-40 p-3.5 rounded-full fixed md:hidden right-3 bottom-28 sm:bottom-24 bg-primary text-white hover:bg-primary-dark outline-none ${
+          className={`z-40 p-3.5 rounded-full fixed md:hidden right-3 bottom-24 bg-primary text-white hover:bg-primary-dark outline-none ${
             show ? "translate-y-0 opacity-100" : "translate-y-36 opacity-0"
           } transition-all ease-in-out duration-500`}
         >
