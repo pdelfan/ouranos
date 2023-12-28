96596e8e85845486c4e69ef940c3af2a53e92893
Pouria Delfanazari <pdelfan@gmail.com>
2023-12-28 12:09:07 -0800

    Fix feed skeleton design inconsistencies

--- src/components/contentDisplay/feedList/FeedListSkeleton.tsx
+++ src/components/contentDisplay/feedList/FeedListSkeleton.tsx
@@ -4,14 +4,14 @@ function Skeleton() {
       <div className="flex flex-wrap gap-3 items-center">
         <div className="rounded-lg bg-gray-200 h-10 w-10" />
         <div className="flex flex-col gap-3">
-          <div className="bg-gray-200 w-20 h-3" />
-          <div className="bg-gray-200 w-32 h-3" />
+          <div className="bg-gray-200 rounded-full w-20 h-3" />
+          <div className="bg-gray-200 rounded-full w-32 h-3" />
         </div>
       </div>
-      <div className="bg-gray-200 w-4/5 h-2 mt-2" />
-      <div className="bg-gray-200 w-3/4 h-2" />
-      <div className="bg-gray-200 w-5/6 h-2" />
-      <div className="bg-gray-200 w-16 h-2 mt-2" />
+      <div className="bg-gray-200 rounded-full w-4/5 h-2 mt-2" />
+      <div className="bg-gray-200 rounded-full w-3/4 h-2" />
+      <div className="bg-gray-200 rounded-full w-5/6 h-2" />
+      <div className="bg-gray-200 rounded-full w-16 h-2 mt-2" />
     </article>
   );
 }
