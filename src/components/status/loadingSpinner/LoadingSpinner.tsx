f759c347f951202b866b24b6f58dc3f8ece2b65e
Pouria Delfanazari <pdelfan@gmail.com>
2024-01-18 17:33:18 -0800

    Clean up loading and skeleton components

--- src/components/status/loadingSpinner/LoadingSpinner.tsx
+++ src/components/status/loadingSpinner/LoadingSpinner.tsx
@@ -2,8 +2,10 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function LoadingSpinner() {
   return (
-    <div className="flex flex-1 justify-center mt-3 animate-spin">
-      <AiOutlineLoading3Quarters className="text-xl" />
+    <div className="flex flex-1 justify-center mt-3">
+      <div className="animate-spin">
+        <AiOutlineLoading3Quarters className="text-xl" />
+      </div>
     </div>
   );
 }
