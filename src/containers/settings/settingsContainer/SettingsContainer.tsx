faebbfc44db6bcf218241238414c2517b491801a
Pouria Delfanazari <pdelfan@gmail.com>
2024-01-23 19:17:10 -0800

    Update feed icon on settings

--- src/containers/settings/settingsContainer/SettingsContainer.tsx
+++ src/containers/settings/settingsContainer/SettingsContainer.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 import { FaSlidersH } from "react-icons/fa";
 import { ImBubbles2 } from "react-icons/im";
 import {
-  BiCloud,
   BiLogoGithub,
   BiSolidCheckCircle,
+  BiSolidCloud,
   BiSolidEnvelope,
   BiSolidXCircle,
 } from "react-icons/bi";
@@ -98,7 +98,7 @@ export default async function SettingsContainer() {
             href="/dashboard/settings/my-feeds"
             className="flex items-center gap-2 p-3 border border-x-0 md:border-x md:first:rounded-t-2xl md:last:rounded-b-2xl last:border-b even:[&:not(:last-child)]:border-b-0 odd:[&:not(:last-child)]:border-b-0 hover:bg-neutral-50"
           >
-            <BiCloud className="text-neutral-600 text-xl" />
+            <BiSolidCloud className="text-neutral-600 text-xl" />
             My Feeds
           </Link>
         </div>
