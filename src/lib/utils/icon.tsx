e2d9925e8312292dedc075868b962e13a51160ff
Pouria Delfanazari <pdelfan@gmail.com>
2024-03-12 17:08:48 -0700

    Add missing theme styles to components

--- src/lib/utils/icon.tsx
+++ src/lib/utils/icon.tsx
@@ -13,34 +13,40 @@ import { IoCloudOffline } from "react-icons/io5";
 export function getAlertIcon(variant: Alert) {
   switch (variant) {
     case "success":
-      return <FaCircleCheck className="text-2xl" />;
+      return <FaCircleCheck className="text-skin-icon-base text-2xl" />;
     case "error":
-      return <HiMiniShieldExclamation className="text-2xl" />;
+      return (
+        <HiMiniShieldExclamation className="text-skin-icon-base text-2xl" />
+      );
     case "info":
-      return <PiWarningCircleFill className="text-2xl" />;
+      return <PiWarningCircleFill className="text-skin-icon-base text-2xl" />;
     case "warning":
-      return <PiWarningCircleFill className="text-2xl" />;
+      return <PiWarningCircleFill className="text-skin-icon-base text-2xl" />;
     default:
-      return <PiWarningCircleFill className="text-2xl" />;
+      return <PiWarningCircleFill className="text-skin-icon-base text-2xl" />;
   }
 }
 
 export function getFeedAlertIcon(variant: FeedAlert) {
   switch (variant) {
     case "empty":
-      return <PiWarningCircleFill className="text-2xl" />;
+      return <PiWarningCircleFill className="text-skin-icon-base text-2xl" />;
     case "doesNotExist":
-      return <PiWarningCircleFill className="text-2xl" />;
+      return <PiWarningCircleFill className="text-skin-icon-base text-2xl" />;
     case "misconfigured":
-      return <HiMiniShieldExclamation className="text-2xl" />;
+      return (
+        <HiMiniShieldExclamation className="text-skin-icon-base text-2xl" />
+      );
     case "badResponse":
-      return <BiSolidErrorAlt className="text-2xl" />;
+      return <BiSolidErrorAlt className="text-skin-icon-base text-2xl" />;
     case "offline":
-      return <IoCloudOffline className="text-2xl" />;
+      return <IoCloudOffline className="text-skin-icon-base text-2xl" />;
     case "blocked":
-      return <HiMiniShieldExclamation className="text-2xl" />;
+      return (
+        <HiMiniShieldExclamation className="text-skin-icon-base text-2xl" />
+      );
     default:
-      return <PiWarningCircleFill className="text-2xl" />;
+      return <PiWarningCircleFill className="text-skin-icon-base text-2xl" />;
   }
 }
 
