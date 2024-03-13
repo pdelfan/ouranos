ca7aaae6df67b8d1816aa26828b45f3d279882dd
Pouria Delfanazari <pdelfan@gmail.com>
2024-03-12 17:46:17 -0700

    Adjust dim theme to use more purple colours

--- src/lib/store/local.ts
+++ src/lib/store/local.ts
@@ -5,5 +5,5 @@ export enum LocalStorage {
 export const THEMES: Theme[] = [
   { label: "Light", value: "light" },
   { label: "Dim", value: "dim" },
-  { label: "Midnight", value: "midnight" },
+  { label: "Dark", value: "dark" },
 ];
