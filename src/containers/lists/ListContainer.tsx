5d54a86713d3473b8f864e6607634af0817d942f
Pouria Delfanazari <pdelfan@gmail.com>
2024-04-28 16:11:58 -0700

    Fix typo

--- src/containers/lists/ListContainer.tsx
+++ src/containers/lists/ListContainer.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 export default function ListContainer(props: Props) {
   const { uri } = props;
-  const [currenTab, setCurrentTab] = useState<"posts" | "members">("posts");
+  const [currentTab, setCurrentTab] = useState<"posts" | "members">("posts");
 
   const handleTabChange = (tab: "posts" | "members") => {
     setCurrentTab(tab);
@@ -24,14 +24,14 @@ export default function ListContainer(props: Props) {
         role="tablist"
         aria-orientation="horizontal"
         className={`no-scrollbar border-skin-base flex flex-nowrap gap-3 overflow-auto border border-t-0 ${
-          currenTab === "posts" ? "border-b" : "border-b-0"
+          currentTab === "posts" ? "border-b" : "border-b-0"
         } px-3 pt-3`}
       >
         <button
           role="tab"
           onClick={() => handleTabChange("posts")}
           className={`border-b-3 hover:text-primary shrink-0 cursor-pointer px-3 pb-2 font-semibold ${
-            currenTab === "posts"
+            currentTab === "posts"
               ? "text-primary border-primary"
               : "text-skin-secondary border-transparent"
           }`}
@@ -42,7 +42,7 @@ export default function ListContainer(props: Props) {
           role="tab"
           onClick={() => handleTabChange("members")}
           className={`border-b-3 hover:text-primary shrink-0 cursor-pointer px-3 pb-2 font-semibold ${
-            currenTab === "members"
+            currentTab === "members"
               ? "text-primary border-primary"
               : "text-skin-secondary border-transparent"
           }`}
@@ -50,8 +50,8 @@ export default function ListContainer(props: Props) {
           Members
         </button>
       </div>
-      {currenTab === "posts" && <FeedContainer feed={uri} mode="list" />}
-      {currenTab === "members" && <ListMembersContainer list={uri} />}
+      {currentTab === "posts" && <FeedContainer feed={uri} mode="list" />}
+      {currentTab === "members" && <ListMembersContainer list={uri} />}
     </section>
   );
 }
