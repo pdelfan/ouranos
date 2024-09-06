5a6432c4020046b71af4c9f4bbf4f208864ae943
Pouria Delfanazari <pdelfan@gmail.com>
2024-09-06 09:03:39 -0700

    Add "Go Home" link to Not Found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,16 @@
+"use client";
+
+import Button from "@/components/actions/button/Button";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
+  const router = useRouter();
+
   return (
     <main className="flex h-[100svh] flex-col items-center justify-center p-3">
       <div className="flex items-center gap-3">
-        <Image
-          src="/ouranos.svg"
-          alt="Ouranos logo"
-          width={40}
-          height={40}
-          className="block transition-transform duration-700 ease-in-out hover:rotate-180"
-        />
+        <Image src="/ouranos.svg" alt="Ouranos logo" width={40} height={40} />
         <Image
           src="/ouranosText.svg"
           alt="Ouranos text"
@@ -21,6 +21,9 @@ export default function NotFound() {
       <h1 className="mt-2 text-center text-lg sm:text-xl">
         The page you are looking for could not be found
       </h1>
+      <div className="mt-6 flex justify-center">
+        <Button onClick={() => router.push("/dashboard/home")}>Go Home</Button>
+      </div>
     </main>
   );
 }
