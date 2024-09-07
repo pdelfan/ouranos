475310777388e22065ef3b69144a8d75427d4977
Pouria Delfanazari <pdelfan@gmail.com>
2024-09-07 10:46:29 -0700

    Adjust max upload image size to match the limit

--- src/lib/utils/image.ts
+++ src/lib/utils/image.ts
@@ -18,7 +18,7 @@ export function getAvatarSize(size?: AvatarSize): number[] {
 export async function compressImage(image: UploadImage) {
   try {
     const compressed = await imageCompression(image, {
-      maxSizeMB: 1,
+      maxSizeMB: 0.9,
       maxWidthOrHeight: 3000,
       fileType: "image/jpeg",
       initialQuality: 0.9,
