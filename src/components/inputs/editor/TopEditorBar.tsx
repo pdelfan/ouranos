6b0eceb41d0fab6b6e2504ac31722b984a8a8b1c
Pouria Delfanazari <pdelfan@gmail.com>
2024-09-13 22:09:43 -0700

    Replace discard with inline reveal

--- src/components/inputs/editor/TopEditorBar.tsx
+++ src/components/inputs/editor/TopEditorBar.tsx
@@ -1,4 +1,3 @@
-import * as Dialog from "@radix-ui/react-dialog";
 import Button from "@/components/actions/button/Button";
 import { UseMutationResult } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
@@ -45,68 +44,71 @@ export default function TopEditorBar(props: Props) {
 
   return (
     <div className="flex flex-wrap items-center justify-between gap-2">
-      <Button
-        onClick={onCloseEditor}
-        className="hover:bg-skin-secondary border-skin-base text-skin-base rounded-full border px-4 py-2 text-sm font-semibold"
-      >
-        Cancel
-      </Button>
-
-      {label !== "" && (
-        <Button
-          onClick={onRemoveLabel}
-          className="hover:bg-skin-inverted bg-skin-tertiary text-skin-base hover:text-skin-inverted rounded-full px-4 py-2 text-sm font-semibold"
-        >
-          <HiOutlineShieldExclamation className="text-xl" />
-          {label === "nsfw"
-            ? "Porn"
-            : label.charAt(0).toUpperCase() + label.slice(1)}
-        </Button>
-      )}
-      <Button
-        onClick={() => {
-          onPublish.mutate(undefined, {
-            onSuccess: () => {
-              onClose();
-            },
-          });
-        }}
-        className={`bg-primary hover:bg-primary-dark rounded-full px-4 py-2 text-sm font-semibold text-white ${
-          onPublish.isPending && "animate-pulse animate-duration-1000"
-        }`}
-        disabled={onPublish.isPending}
-      >
-        <BiLogoTelegram className="text-xl" />
-        {onPublish.isPending ? "Posting..." : "Post"}
-      </Button>
-      <Dialog.Root open={showCancelModal} onOpenChange={setShowCancelModal}>
-        <Dialog.Portal>
-          <Dialog.Overlay className="bg-skin-overlay-muted fixed inset-0 z-50 h-screen w-screen" />
-          <Dialog.Content className="bg-skin-base border-skin-base fixed left-[50%] top-[50%] z-50 h-fit max-h-[90svh] w-[90svw] max-w-sm translate-x-[-50%] translate-y-[-50%] overflow-auto rounded-2xl border p-3 shadow-2xl">
-            <h2 className="text-skin-base mb-2 text-xl font-semibold">
-              Discard Draft
-            </h2>
-            <p className="text-skin-base">Do you want to discard this draft?</p>
-            <div className="mt-2 flex justify-end gap-2">
-              <Dialog.Close
-                className="text-skin-base border-skin-base hover:bg-skin-secondary rounded-full border px-4 py-2 text-sm font-semibold"
-                onClick={() => editor.commands.focus()}
-              >
-                Cancel
-              </Dialog.Close>
-              <Button
-                className="bg-primary hover:bg-primary-dark text-skin-icon-inverted rounded-full px-4 py-2 text-sm font-semibold"
-                onClick={() => {
-                  setShowCancelModal(false);
+      {!showCancelModal && (
+        <>
+          {" "}
+          <Button
+            onClick={onCloseEditor}
+            className="hover:bg-skin-secondary border-skin-base text-skin-base rounded-full border px-4 py-2 text-sm font-semibold"
+          >
+            Cancel
+          </Button>
+          {label !== "" && (
+            <Button
+              onClick={onRemoveLabel}
+              className="hover:bg-skin-inverted bg-skin-tertiary text-skin-base hover:text-skin-inverted rounded-full px-4 py-2 text-sm font-semibold"
+            >
+              <HiOutlineShieldExclamation className="text-xl" />
+              {label === "nsfw"
+                ? "Porn"
+                : label.charAt(0).toUpperCase() + label.slice(1)}
+            </Button>
+          )}
+          <Button
+            onClick={() => {
+              onPublish.mutate(undefined, {
+                onSuccess: () => {
                   onClose();
-                }}
-              >
-                Discard
-              </Button>
-            </div>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
+                },
+              });
+            }}
+            className={`bg-primary hover:bg-primary-dark rounded-full px-4 py-2 text-sm font-semibold text-white ${
+              onPublish.isPending && "animate-pulse animate-duration-1000"
+            }`}
+            disabled={onPublish.isPending}
+          >
+            <BiLogoTelegram className="text-xl" />
+            {onPublish.isPending ? "Posting..." : "Post"}
+          </Button>
+        </>
+      )}
+      {showCancelModal && (
+        <div className="flex flex-wrap items-center justify-between gap-6">
+          <h2 className="text-skin-base text-lg font-medium">
+            Do you want to discard this draft?
+          </h2>
+          <div className="flex justify-end gap-2">
+            <Button
+              className="text-skin-base border-skin-base hover:bg-skin-secondary rounded-full border px-4 py-2 text-sm font-semibold"
+              onClick={() => {
+                setShowCancelModal(false);
+                editor.commands.focus();
+              }}
+            >
+              Go back
+            </Button>
+            <Button
+              className="bg-primary hover:bg-primary-dark rounded-full px-4 py-2 text-sm font-semibold text-white"
+              onClick={() => {
+                setShowCancelModal(false);
+                onClose();
+              }}
+            >
+              Discard
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
