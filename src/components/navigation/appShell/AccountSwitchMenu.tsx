ef6ac7a4edf4d51654130e0fb34d43967eef39ef
Pouria Delfanazari <pdelfan@gmail.com>
2025-01-30 17:29:42 -0800

    fix: truncate name and handle in account switch

--- src/components/navigation/appShell/AccountSwitchMenu.tsx
+++ src/components/navigation/appShell/AccountSwitchMenu.tsx
@@ -20,18 +20,18 @@ export default async function AccountSwitchMenu() {
   const profile = await getProfile({ handleOrDid: session.did });
 
   return (
-    <Group justify="space-between" px={"md"} py={"xs"} wrap="nowrap" gap={"md"}>
-      <Group gap={"sm"} wrap="nowrap">
-        <Avatar
-          component="a"
-          href={`/dashboard/user/${profile.handle}`}
-          src={profile.avatar?.replace("avatar", "avatar_thumbnail") ?? null}
-          alt={`${profile.handle}s avatar`}
-          name={profile.handle}
-          color="purple"
-          variant="filled"
-        />
-        <Stack gap={0} w={170}>
+    <Group gap={"sm"} wrap="nowrap" px={"md"} py={"xs"} align="flex-start">
+      <Avatar
+        component="a"
+        href={`/dashboard/user/${profile.handle}`}
+        src={profile.avatar?.replace("avatar", "avatar_thumbnail") ?? null}
+        alt={`${profile.handle}s avatar`}
+        name={profile.handle}
+        color="purple"
+        variant="filled"
+      />
+      <Box w={"80%"}>
+        <Stack gap={0}>
           {profile.displayName && (
             <Text fw={500} size="sm" truncate="end">
               {profile.displayName}
@@ -39,26 +39,29 @@ export default async function AccountSwitchMenu() {
           )}
 
           <Text c="gray" size="sm" truncate="end">
-            spouriawefawfawfawfwfade.coms
+            {profile.handle}
           </Text>
         </Stack>
-      </Group>
-      <Menu shadow="md" radius={"md"}>
-        <MenuTarget>
-          <ActionIcon variant="light" color="gray" radius={"xl"}>
-            <BsThreeDots />
-          </ActionIcon>
-        </MenuTarget>
-        <MenuDropdown>
-          <MenuItem
-            component="a"
-            href="/api/auth/logout"
-            leftSection={<BiLogOut />}
-          >
-            Sign out
-          </MenuItem>
-        </MenuDropdown>
-      </Menu>
+      </Box>
     </Group>
   );
+}
+
+{
+  /* <Menu shadow="md" radius={"md"}>
+  <MenuTarget>
+    <ActionIcon variant="light" color="gray" radius={"xl"}>
+      <BsThreeDots />
+    </ActionIcon>
+  </MenuTarget>
+  <MenuDropdown>
+    <MenuItem
+      component="a"
+      href="/api/auth/logout"
+      leftSection={<BiLogOut />}
+    >
+      Sign out
+    </MenuItem>
+  </MenuDropdown>
+</Menu> */
 }
